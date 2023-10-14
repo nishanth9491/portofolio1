6998cdb38b01ead836eb3282f181238e711a3962
nishanth <nishanthreddy4566@gmail.com>
2023-10-14 21:56:34 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,108 @@
+let resumeData = {
+  "imagebaseurl": "https://your-image-url.com",
+  "name": "Nishanth Reddy",
+  "role": "Programmer",
+  "linkedinId": "Your LinkedIn Id",
+  "skypeid": "Your Skype Id",
+  "roleDescription": "A dynamic and versatile learner, I am able to quickly adapt to new situations and assimilate new information. I approach learning with enthusiasm and determination, always seeking to expand my skill set.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/your-linkedin-profile",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/your-github-username",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "skype",
+      "url": "skype:your-skype-id?chat",
+      "className": "fa fa-skype"
+    }
+  ],
+  "aboutme": "A dynamic and versatile learner, I am able to quickly adapt to new situations and assimilate new information. I approach learning with enthusiasm and determination, always seeking to expand my skill set.",
+  "address": "Vijayawada, Andhra Pradesh, India",
+  "website": "https://your-website-url.com",
+  "education": [
+    {
+      "UniversityName": "Your University/Institute Name",
+      "specialization": "B.Tech in Computer Science",
+      "MonthOfPassing": "Your Month",
+      "YearOfPassing": "Your Year",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "UniversityName": "Your University/Institute Name",
+      "specialization": "Some specialization",
+      "MonthOfPassing": "Your Month",
+      "YearOfPassing": "Your Year",
+      "Achievements": "Some Achievements"
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "Your Company Name",
+      "specialization": "Some specialization",
+      "MonthOfLeaving": "Your Month",
+      "YearOfLeaving": "Your Year",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "CompanyName": "Your Company Name",
+      "specialization": "Some specialization",
+      "MonthOfLeaving": "Your Month",
+      "YearOfLeaving": "Your Year",
+      "Achievements": "Some Achievements"
+    }
+  ],
+  "skillsDescription": "Your skills here",
+  "skills": [
+    {
+      "skillname": "Java"
+    },
+    {
+      "skillname": "Python"
+    },
+    {
+      "skillname": "HTML and CSS"
+    },
+    {
+      "skillname": "JavaScript"
+    },
+    {
+      "skillname": "English"
+    },
+    {
+      "skillname": "Telugu"
+    },
+    {
+      "skillname": "Hindi"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "Emails to Speech (Python)",
+      "description": "Coder",
+      "imgurl": "images/portfolio/project1.jpg"
+    },
+    {
+      "name": "Fake News Detector (ML)",
+      "description": "Coded",
+      "imgurl": "images/portfolio/project2.jpg"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+};
+
+export default resumeData;
